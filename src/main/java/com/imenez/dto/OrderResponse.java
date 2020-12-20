76a7b3901faacec03561c6f6e6896be5c1149ac5
Enes Temel <enes.temel@vodafone.com>
2020-12-20 11:21:46 +0300

    order response class

---
+++ src/main/java/com/imenez/dto/OrderResponse.java
@@ -0,0 +1,14 @@
+package com.imenez.dto;
+
+public class OrderResponse {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
